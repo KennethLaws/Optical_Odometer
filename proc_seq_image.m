48ee7373916e0bdf73d3fe0a1ee433ed866bdc28
KennethLaws <kenneth.laws@here.com>
2017-11-16 16:21:31 -0800

    more code cleanup

--- proc_seq_image.m
+++ proc_seq_image.m
@@ -14,12 +14,13 @@ camera = 'BLFY-PGE-20E4C-CS';
 lens = '8MM 1/1.8 ir mp';   % this lens has much lower distortion than previous
 
 % define a subframe (smaller than maximum)
-w = 400;    % width of subframe
-h = 200;    % height of subframe
-x1 = 450;   % bottom left corner of initial subframe at top center of pix frame
-y1 = 1300;
-xRes = 1200;    % resolution of camera image (hard coded for speed)
-yRes = 1600;
+imageRes = [1600, 1200];
+w = 256;    % width of subframe
+h = 128;    % height of subframe
+xPix = 1024;    % matrix dimensions for image processing factor of 2^n
+yPix = 1024;
+x1 = (imageRes(2) - w)/2;
+y1 = imageRes(1) - h;
 
 % set the file names
 imgFile1 = '../../images/Driveway2/10172917_run4_2017-10-17-100449-0000.pgm';
@@ -32,11 +33,16 @@ et1=toc;
 
 % process image pair
 tic;
-[ypeak, xpeak, c, max_c] = image_reg(yRes,xRes,image_2,subFrame1);
+[ypeak, xpeak, c, max_c] = image_reg(yPix,xPix,image_2,subFrame1);
 et = toc;
 
 % compute shift
 deltPosPix = [y1 - ypeak,x1 - xpeak];
+
+%******************************************************
+
+
+% generate plots and outputs
 
 % plot the first image
 figure(1), clf, hold on, colormap gray
@@ -47,7 +53,7 @@ shading interp;
 plotrect(x1,y1,w,h,1);
 
 %plot a registration line
-plot([800, 1200],[1300 1300],'r');
+plot([800, 1200],[y1 y1],'r');
 
 % plot the second image
 figure(2), clf, hold on, colormap gray
@@ -59,15 +65,16 @@ shading interp;
 plot([800, 1200],[ypeak-4 ypeak-4],'r');
 
 %plot the original registration line
-plot([800, 1200],[1300 1300],'r');
+plot([800, 1200],[y1 y1],'r');
 
 % print results
 fprintf('Camera = %s\n',camera);
 fprintf('installed lens = %s\n', lens);
-fprintf('image size: %d x %d\n',yRes,xRes);
+fprintf('image size: %d x %d\n',size(image_1));
 fprintf('*************************************************\n');
 fprintf('template size: %d x %d \n',w,h);
-fprintf('template lower left corner position: (%d, %d)\n', x1,y1);
+fprintf('template lower left corner position: (%d, %d)\n', y1,x1);
+fprintf('processing matrix dimensions: (%d, %d)\n',yPix,xPix);
 fprintf('retrieved position: (%d, %d)\n',xpeak,ypeak);
 fprintf('retrieved position shift: dy = %d pix, dx = %d pix\n',deltPosPix);
 fprintf('reading files took %0.3E sec\n',et1);
