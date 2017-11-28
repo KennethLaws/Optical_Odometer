486e1ec3f36ec0697af3f51d4b11c2cfa95f451a
Kenneth Laws <kenneth.laws@here.com>
2017-11-28 12:13:04 -0800

    adds some basic c stuff

---
+++ speed_test.c
@@ -0,0 +1,19 @@
+/*	Program to test speed improvement going from Matlab to C for 
+	phase correlation in frequency domain for image registration
+
+	by Kenneth Laws
+	Here technologies
+	11/28/2017
+
+*/
+
+#include <stdio.h>
+
+int main(void){
+
+	printf("Hi there\n");
+
+	return 0;	
+
+
+}
