68cf4795a8f1a3faa58609acba503befb350f2bb
Kenneth Laws <kenneth.laws@here.com>
2017-12-01 17:22:43 -0800

    improved Makefile and moved some files

--- src/speed_test.c
+++ src/speed_test.c
@@ -63,7 +63,7 @@ uint16_t ncolors=0,pixDepth=0;
 int main(void){
 
 
-	fp = fopen("./sample_image/Image__2017-11-28__10-16-16.bmp", "rb");
+	fp = fopen("../sample_image/Image__2017-11-28__10-16-16.bmp", "rb");
 
 
 	// read in the basic information header (14 bytes)
